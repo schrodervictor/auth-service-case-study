26ca70bde0405dccdab51d04a1ecf552ef0eb04b
Victor Schroeder <me@vschroeder.net>
2026-02-21 22:31:14 +0000

    style: fix lint error in migration class declaration

--- src/migrations/1740100000000-CreateRefreshTokensTable.ts
+++ src/migrations/1740100000000-CreateRefreshTokensTable.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateRefreshTokensTable1740100000000
-    implements MigrationInterface
-{
+export class CreateRefreshTokensTable1740100000000 implements MigrationInterface {
     name = 'CreateRefreshTokensTable1740100000000';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
